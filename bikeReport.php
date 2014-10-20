813b8d5e4935d07d4540dbaa15bb5356d5406021
Brian Neary <bri.neary@gmail.com>
2014-10-20 19:16:54 -0400

    Weekly forecast, wind compass direction, round numbers

--- bikeReport.php
+++ bikeReport.php
@@ -20,6 +20,46 @@ function clean($str)
 	$str = htmlentities($str, ENT_QUOTES, "UTF-8");
 	return $str;
 }
+
+function compass($degrees)
+{
+	$compass = array("N","NNE","NE","ENE","E","ESE","SE","SSE","S","SSW","SW","WSW","W","WNW","NW","NNW");
+	$compcount = round($degrees / 22.5);
+	$compdir = $compass[$compcount];
+	return $compdir;
+}
+
+function reportWeekly($week)
+{
+	$weekdays = array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+	$unixDay = mktime();
+	$today = date('N', $unixDay); // Returns 1-7
+
+	print "<p><b>Weekly Summary</b></p>";
+
+	for($i = 0; $i <= 6; $i++)
+	{
+		if($today <= 6)
+		{
+			print "<p>";
+			print "<i>" . $weekdays[$today] . "</i>: " . $week[$i][0] . "<br/>";
+			print "Wind speed/bearing: " . $week[$i][1] . " / " . $week[$i][2] . "<br/>";
+			print "Precipitation: " . $week[$i][3][0] . " / " . $week[$i][3][1] . " / " . $week[$i][3][2] . "<br/>";
+			print "</p>";
+		}
+		else
+		{
+			// We've gone off the end off the array
+			print "<p>";
+			print "<i>" . $weekdays[$today - 7] . "</i>: " . $week[$i][0] . "<br/>";
+			print "Wind speed/bearing: " . $week[$i][1] . " / " . $week[$i][2] . "<br/>";
+			print "Precipitation: " . $week[$i][3][0] . " / " . $week[$i][3][1] . " / " . $week[$i][3][2] . "<br/>";
+			print "</p>";
+		}
+		$today++;
+	}
+}
+
 // Acquire API credentials from an ini file.
 $ini_array = parse_ini_file("bikereport.ini", true);
 
@@ -77,6 +117,48 @@ $nextDayForecast = $json->daily->data[0]->summary;
 $nextDayTempMax = $json->daily->data[0]->temperatureMax;
 $nextDayTempMin = $json->daily->data[0]->temperatureMin;
 
+/*
+	Associative Array for storing the next week's weather.  Each member represents a day.
+	[i]: The daily record array (whole)
+	[i][0]: Summary for weather on day i
+	[i][1]: Wind Speed
+	[i][2]: Wind Bearing
+	[i][3]: Precipitation: 
+		[i][3][0]: intensity
+		[i][3][1]: max
+		[i][3][2]: probability
+	[i][4]: Temperature(whole): 
+		[i][4][0]: tempMin
+		[i][4][1]: tempMax
+		[i][4][2]: apparentTempMin
+		[i][4][3]: apparentTempMax
+
+*/
+$weeklyWeather = array();
+
+// Array for temp use in building $weeklyWeather
+$dailyWeather = array();
+
+for($i = 0; $i < 7; $i++)
+{
+	$dailyWeather[0] = $json->daily->data[$i]->summary;
+	$dailyWeather[1] = $json->daily->data[$i]->windSpeed;
+	$dailyWeather[2] = $json->daily->data[$i]->windBearing;
+
+	$dailyWeather[3][0] = $json->daily->data[$i]->precipIntensity;
+	$dailyWeather[3][1] = $json->daily->data[$i]->precipIntensityMax;
+	$dailyWeather[3][2] = $json->daily->data[$i]->precipProbability;
+
+	$dailyWeather[4][0] = $json->daily->data[$i]->temperatureMin;
+	$dailyWeather[4][1] = $json->daily->data[$i]->temperatureMax;
+	$dailyWeather[4][2] = $json->daily->data[$i]->apparentTemperatureMin;
+	$dailyWeather[4][3] = $json->daily->data[$i]->apparentTemperatureMax;
+
+	// Push the temp on, then clear it
+	array_push($weeklyWeather, $dailyWeather);
+	$dailyWeather = array();
+}
+
 $output = "";
 $output .= "<html>
  <head>
@@ -92,14 +174,19 @@ $output .= "<html>
 $output .= "<p><b>Data:</b> " . $cityName . ", " . $state . ", " . $country . ", " . $lat . ", " . $lng .  ", " . $units . "</p>";
 $output .= "<p><b>Time: </b>" . time() . "</p>";
 $output .= "<p><b>Present Conditions:</b> " . $currently . "</p>";
-$output .= "<p><b>Temperature: </b>" . $temperature . " " . $tempSuffix . "</p>";
-$output .= "<p><b>Wind Speed / Bearing:</b> " . $windSpeed . " " . $speedSuffix . " / " . $windBearing . " degrees</p>";
+$output .= "<p><b>Temperature: </b>" . round($temperature) . " " . $tempSuffix . "</p>";
+$output .= "<p><b>Wind Speed / Bearing:</b> " . round($windSpeed) . " " . $speedSuffix . " / " . $windBearing . " degrees (" . compass($windBearing) . ")</p>";
 
 $output .= "<p><b>Time Tomorrow: </b>" . (time() + 86400) . "</p>";
 $output .= "<p><b>24hr Forecast: </b>" . $nextDayForecast . "</p>";
-$output .= "<p><b>High / Low tomorrow: </b>" . $nextDayTempMax . " " . $tempSuffix . " / " . $nextDayTempMin . $tempSuffix . "</b>";
+$output .= "<p><b>High / Low tomorrow: </b>" . round($nextDayTempMax) . " " . $tempSuffix . " / " . round($nextDayTempMin) . $tempSuffix . "</b>";
 
 $output .= "<p><b>Weekly Forecast: </b>" . $weeklyForecast . "</p>";
+print $output;
+$output = "";
+
+// Report the week's weather
+reportWeekly($weeklyWeather);
 
 $output .= "</body></html>";
 print $output;
